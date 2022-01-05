77ec0197ad9a49df8d076e9638e786e60171a9d6
RUweN <ruwen.sadocco@t-online.de>
2022-01-05 17:27:10 +0100

    Added help window

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pynput
 import keyboard
 import json
 import tkinter
+import threading
 
 from tkinter import filedialog
 from tkinter import messagebox
 from macro_parser import parse_file
+
+
+PROGRAM_NAME = "Macro Everything"
+VERSION = "0.1"
 
 
 def parse_config():
@@ -18,6 +23,8 @@ def get_hotkeys_dict(conf):
     ret = dict()
     ret[conf["emergency_stop"]] = stop
     ret[conf["start_last"]] = start_last
+    ret[conf["restart_last"]] = restart_last
+    ret[conf["stop_macro"]] = stop_macro
     ret[conf["toggle_recording"]] = toggle_record
     ret[conf["choose_macro"]] = select_macro
 
@@ -33,6 +40,14 @@ def start_last():
     print("starting last used macro")
 
 
+def restart_last():
+    print("restarting macro")
+
+
+def stop_macro():
+    print("Stopping current macro")
+
+
 def select_macro():
     # Not implemented
     print("Selection Dialog")
@@ -43,8 +58,8 @@ def select_macro():
 
     if not file_path.endswith(".macro"):
         alert("Bad file!", "You have to select a .macro file!")
-
-    parse_file(file_path)
+    else:
+        parse_file(file_path)
 
 
 def toggle_record():
@@ -59,9 +74,37 @@ def alert(title, msg):
     root.destroy()
 
 
+class HelpWindow(threading.Thread):
+    def __init__(self, conf):
+        super().__init__()
+        self.conf = conf
+
+    def run(self):
+        root = tkinter.Tk()
+        root.title(f"{PROGRAM_NAME} - v{VERSION}")
+        root.configure(background="white")
+        root.grid_rowconfigure(10, weight=1)
+        for i, x in enumerate(self.conf):
+            if x == "last_used":
+                break
+            a = tkinter.Label(root, text=x, bg="white")
+            a.config(font=("Calibri", 14, "bold"))
+            a.grid(row=i, column=0)
+
+            b = tkinter.Label(root, text=self.conf[x], bg="white")
+            b.config(font=("Calibri", 14, "bold"))
+            b.grid(row=i, column=2, padx=10)
+
+        root.mainloop()
+
+
 config = parse_config()
 
 BASE_PATH = config["base_path"]
+
+help_window = HelpWindow(config)
+help_window.daemon = True
+help_window.start()
 
 with pynput.keyboard.GlobalHotKeys(get_hotkeys_dict(config)) as hotkeys:
     hotkeys.join()
